4bd0514c50beb7755d3cbdf74ae70017d056ee0b
chenyanyang <184536632@qq.com>
2022-01-22 16:37:34 +0800

    init

---
+++ Tello.js
@@ -0,0 +1,128 @@
+
+
+class Tello {
+
+	constructor(){
+		this.tello_address = '192.168.10.1';
+		this.command_port = 8889;
+
+		const dgram = require('dgram');
+		this.command_socket = dgram.createSocket('udp4');
+		this.initCommandSocket();
+	}
+
+	// function for handling incoming messages & errors from tello
+	initCommandSocket(){
+		this.command_socket.on('message', (msg, rinfo) => {
+			console.log(`got ${msg}`)
+		});
+		this.command_socket.on('error', (err) => {
+				console.log(`error: ${err}\n closing socket`);
+				this.command_socket.close();
+		});
+		this.command_socket.on('listening', () => {
+			let address = this.command_socket.address();
+			console.log(`established connection with ${address.address}:${address.port}`)
+		});
+		this.command_socket.bind(this.command_port);
+	}
+
+	// function for sending commands to tello
+	sendCommand(message){
+		var command = new Buffer.from(message);
+		this.command_socket.send(command, 0, command.length, this.command_port, this.tello_address, (err, bytes) =>{
+			if(err)
+				console.log(err);
+		})
+	}
+
+	// function to command the drone through command line
+	startCLI(){
+		// first enter SDK mode:
+		this.sendCommand('command');
+		// create CLInterface
+		const readline = require('readline');
+		const rl = readline.createInterface({
+		  input: process.stdin,
+		  output: process.stdout
+		});
+
+		let that = this;
+		rl.on('line', (line) => {
+			// if we type in close, close interface
+			if(line === 'close'){
+				rl.close();
+			}
+			if(line === 'rt'){
+				readline.emitKeypressEvents(process.stdin);
+				process.stdin.setRawMode(true);
+				process.stdin.on('keypress', (str, key) => {
+					if (key.ctrl && key.name === 'c') {
+					process.exit();
+					} else {
+					console.log(`You pressed the "${str}" key`);
+					if(str === 't') {console.log('taking off');that.sendCommand('takeoff')};
+					if(str === 'l') {console.log('landing');that.sendCommand('land')};
+					if(str === 'w') {console.log('forward 20');that.sendCommand('forward 20');}
+					if(str === 's') {console.log('back 20');that.sendCommand('back 20');}
+					if(str === 'a') {console.log('left 20');that.sendCommand('left 20');}
+					if(str === 'd') {console.log('right 20');that.sendCommand('right 20');}
+					if(str === 'u') {console.log('up 20');that.sendCommand('up 20');}
+					if(str === 'j') {console.log('down 20');that.sendCommand('down 20');}
+					if(str === 'h') {console.log('ccw 20');that.sendCommand('ccw 20');}
+					if(str === 'k') {console.log('cw 20');that.sendCommand('cw 20');}
+					if(str === 'g') {console.log('flip forward ');that.sendCommand('flip f');}
+					if(str === 'b') {console.log('flip backward ');that.sendCommand('flip b');}
+					if(str === 'v') {console.log('flip left ');that.sendCommand('flip l');}
+					if(str === 'n') {console.log('flip right ');that.sendCommand('flip r');}
+					//if(str === 'c') {console.log('closing off');that.sendCommand('close');process.exit();}
+					console.log();
+					//console.log(key);
+					console.log();
+					}
+				});
+				
+			}
+			// send command receive from CL to tello
+			that.sendCommand(line);
+		}).on('close', () => {
+			// upon closing interface, terminate program
+			that.command_socket.close();
+			process.exit(0);
+		})
+	}
+
+	  keypress = function(){
+		this.readline.emitKeypressEvents(process.stdin);
+		console.log("in rt func")
+		if (process.stdin.isTTY)
+			process.stdin.setRawMode(true);
+	
+		process.stdin.on('keypress', (chunk, key) => {
+			if (key && key.name == 'q')
+			console.log("q pressed!")
+			//keypress();
+			});
+	
+		process.stdin.on('keypress', (chunk, key) => {
+			if (key && key.name == 'Z')
+			console.log("Z pressed!")
+			rl.close();
+			});
+
+		process.stdin.setRawMode(true);
+		process.stdin.resume();
+	}
+}
+
+
+// init drone and start
+let drone = new Tello();
+drone.startCLI();
+
+// IMPORTANT close socket connection upon exception
+process.on('uncaughtException', () => {
+	drone.command_socket.close();
+});
+
+// ENJOY!
---
+++ syllabus.md
@@ -0,0 +1,54 @@
+# Intro to tello drone (Class 1)
+- The purpose of this drone.
+- What is it capable of (what is not in contrast).
+- Ways to control.
+# Safety (Class 1,2)
+- propeller protect
+- hight limit
+- controll range
+# Terminalogy
+- engine:propeller (Class 1)
+- frame (Class 1)
+- eletronic components (Class 2)
+  - sensors 
+  - cpu and motherboard 
+  - camera
+  - wifi module
+# Control
+- iphone app (Class 1)
+- PC/laptop communication through wifi(Class 2)
+- iPad/iphone communication through wifi(Class 3)
+
+## Control theory (Class 2)
+- close loop control : self stabling 
+- close loop control : thresholding on when to stop moving/rotating 
+- open loop control : accepting signals from wifi
+
+# Motions (Class 1)
+- takeoff and land
+- rotate on z-axis
+- move forward/backward/left/right
+- flip forward/backward/left/right
+
+# Programming (Class 2 or 3)
+- install iSH on ipad
+  - install git, nodejs on iSH
+- How each motion is programmed
+- How can we programme on already programmed motions
+  - move in line/circle/square/cross/triangle or 3D shapes
+  - pricision is dependent on sensors.
+
+# Playing
+## video demo on each of the motions (Class 1)
+
+## indoor (Class 1)
+- takeoff and land (both controll and programming)
+- rotate on z-axis (both controll and programming)
+
+## indoor (Class 2)
+- move forward/backward/left/right (only one step)
+- flip forward/backward/left/right (only one step)
+
+## outdoor (Class 3)
+- move forward/backward/left/right (with limited range)
+- flip forward/backward/left/right (with limited range)
